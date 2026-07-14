3b7f8014e1323a41307301a2f754390a351223b3
Pranav Chougule <chougulepranav31@gmail.com>
2026-07-14 13:26:04 +0530

    ReverseNumberExample

---
+++ ReverseNumber4/ReverseNumber4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumber4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1234;
+            int rev = 0;
+            while (num > 0)
+            {
+                rev = rev * 10 + num%10;
+                num = num/10;
+               
+            }
+            Console.WriteLine(rev);
+        }
+
+    }
+    
+}
